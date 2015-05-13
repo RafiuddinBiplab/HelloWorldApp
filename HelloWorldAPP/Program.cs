5ea74007e8650bd126e3a29476cf4e3265ba50ee
ramcste <lucky.guyram@gmail.com>
2015-05-13 17:21:34 +0600

    This is Basic C# Code where we learn how to write code in C#

---
+++ HelloWorldAPP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine();
+            int aNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(aNumber);
+            Console.ReadKey();
+        }
+    }
+}
